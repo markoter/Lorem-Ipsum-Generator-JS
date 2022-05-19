151c68a2e1be4eb38a4e418c04030bf6eb5465dc
markoter <61563247+markoter@users.noreply.github.com>
2022-05-19 18:15:31 +0200

    generate 1 paragraf if number isnt correct

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 //lorem text
-const text = ['some text for generate'];
+const text = ['some text for generate', 'second para', 'third parag', 'fourth para', '5. p'];
 
 const form = document.querySelector(".lorem-form");
 const amount = document.getElementById("amount");
 const result = document.querySelector(".lorem-text");
 
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault();
     const value = parseInt(amount.value); //without parse it will be string
-    console.log(value);
-})
+    console.log(isNaN(value));
+
+    if (isNaN(value) || value < 0 || value > 9) {
+        result.innerHTML = '<p class="result">${}</p>';
+    }
+});
