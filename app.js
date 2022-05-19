52a7df0726615391356ad74103b1e0928a105d11
markoter <61563247+markoter@users.noreply.github.com>
2022-05-19 17:44:39 +0200

    add event listener

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 //lorem text
-const text = ['some text for generate';]
+const text = ['some text for generate'];
 
 const form = document.querySelector('lorem-form');
 const amount = document.getElementById('amount');
-const result = document.querySelector("lorem-text")
+const result = document.querySelector("lorem-text");
+
+form.addEventListener('submit', function(e) {
+    console.log("hello world!");
+})
