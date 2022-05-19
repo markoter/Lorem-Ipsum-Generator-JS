0de7309238e701526ffbd961bf70b54be947edbf
markoter <61563247+markoter@users.noreply.github.com>
2022-05-19 18:52:03 +0200

    remove debug console logs

--- app.js
+++ app.js
@@ -8,11 +8,9 @@ const result = document.querySelector(".lorem-text");
 form.addEventListener("submit", function (e) {
     e.preventDefault();
     const value = parseInt(amount.value); //without parse it will be string
-    console.log(isNaN(value));
     const random = Math.floor(Math.random() * text.length);
 
     if (isNaN(value) || value < 0 || value > 9) {
-        console.log(random);
         result.innerHTML = `<p class="result">${text[random]}</p>`;
     }
     else {
