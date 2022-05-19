ce1c52f684ba296fee796fcfa8913f466d11f300
markoter <61563247+markoter@users.noreply.github.com>
2022-05-19 18:49:41 +0200

    show random paragraf when number isn't correct

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ form.addEventListener("submit", function (e) {
     e.preventDefault();
     const value = parseInt(amount.value); //without parse it will be string
     console.log(isNaN(value));
+    const random = Math.floor(Math.random() * text.length);
 
     if (isNaN(value) || value < 0 || value > 9) {
-        result.innerHTML = `<p class="result">${text[0]}</p>`;
+        console.log(random);
+        result.innerHTML = `<p class="result">${text[random]}</p>`;
     }
     else {
         let tempText = text.slice(0, value);
