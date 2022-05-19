475afd04fdd89a6ff8d28f91494933f0f2ed8f6b
markoter <61563247+markoter@users.noreply.github.com>
2022-05-19 17:40:30 +0200

    start with js

---
+++ app.js
@@ -0,0 +1,6 @@
+//lorem text
+const text = ['some text for generate';]
+
+const form = document.querySelector('lorem-form');
+const amount = document.getElementById('amount');
+const result = document.querySelector("lorem-text")
