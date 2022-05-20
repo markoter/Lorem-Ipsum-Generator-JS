2fffc53f04b7354e721810bfdd2e004b019c9948
markoter <61563247+markoter@users.noreply.github.com>
2022-05-20 20:34:12 +0200

    add cicero text in file

--- app.js
+++ app.js
@@ -34,3 +34,19 @@ form.addEventListener("submit", function (e) {
         result.innerHTML = tempText;
     }
 });
+
+import readFileSync from 'fs';
+//const {readFileSync, promises: fsPromises} = require('fs');
+
+// ✅ read file SYNCHRONOUSLY
+function syncReadFile(filename) {
+  const contents = readFileSync(filename, 'utf-8');
+
+  const arr = contents.split(/\r?\n/);
+
+  console.log(arr); // 👉️ ['One', 'Two', 'Three', 'Four']
+
+  return arr;
+}
+
+syncReadFile('./cicero.txt');
